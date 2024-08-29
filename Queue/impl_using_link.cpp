f7ddcb8e243e6da74e08c54159a6fe1c011d3702
Rudraksha-007 <rudrakshadwivedi18@gmail.com>
2024-08-29 22:58:48 +0530

    Implemnetation of Q using linkedlists

--- Queue/impl_using_link.cpp
+++ Queue/impl_using_link.cpp
@@ -5,11 +5,53 @@ const
 struct node{
     int data;
     node*next;
+    node(){
+        next=nullptr;
+    }
+    node(int x):data(x),next(nullptr){}
 };
+//note we are not considering size limitations 
 class Q{
     node*front;
     node*rear;
     
     public:
-
+    Q(){
+        front=rear=nullptr;
+    }
+    void enqueue(int);
+    int dequeue();
+    void display();
 };
+void Q::enqueue(int data){//this is basically appending to the linkedlist
+    node*temp=new node(data);
+    if(front==nullptr){
+        front=temp;
+        rear=front;
+    }else{
+        rear->next=temp;
+        rear=temp;
+    }
+}
+int Q::dequeue(){
+    if(front==nullptr){
+        cout<<"Queue is empty\n";
+        return -1;
+    }else{
+        node*togetridof=front;
+        int data=front->data;
+        front=front->next;
+        delete togetridof;
+        return data;
+    }
+}
+void Q::display(){
+    node*it=front;
+    while(it!=nullptr ){
+        cout<<it->data<<"\t";
+        it=it->next;
+    }
+}
+int main(){
+    //todo
+}
